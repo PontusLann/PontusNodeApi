d44681a4d06e7430a34d36fc7353d192e51efc19
PontusLann <pontus.lann@iths.se>
2023-01-23 10:25:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const https = require("https");
+
+const instruments = {Instuments: [{name: "Guitarr", kind: "String Instrument"},
+{name: "Piano", kind: "PlingPlong"},
+{name: "Violin", kind: "Stroke Instrument"}]}
+
+app.get("/joke", (req, res) =>{
+    https.get("https://api.chucknorris.io/jokes/random", (response) =>{
+        response.on("data", (data) =>{
+            res.send(JSON.parse(data))
+        })
+    }).on("error", (err) =>{
+        console.log("There was an error " + err.message)
+    })
+})
+
+app.get("/instruments", (req, res) =>{
+    res.send(instruments)
+})
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.listen(PORT, () =>{
+    console.log("Listening to port " + PORT)
+})
